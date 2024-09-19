04733e784d69722d961b398cf43dc291a72378f0
“Krishan <2620765G@student.gla.ac.uk>
2024-09-19 01:18:00 +0100

    Complete game without computer

--- TIC-TAC-TOE.py
+++ TIC-TAC-TOE.py
@@ -30,6 +30,67 @@ def playerInput(board):
 
 # check for win or tie
 
+def checkHorizontal(board):
+    global winner 
+    if board[0] == board[1] == board[2] and board[2] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board [4] == board[5] and board[4] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[7] != "-":
+        winner = board[6]
+        return True
+    
+def checkRow(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != '-':
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != '-':
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != '-':
+        winner = board[2]
+        return True
+    
+def checkDiagonal(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != '-':
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != '-':
+        winner = board[2]
+        return True
+
+def checkTie(board):
+    global gameRunning
+    if "-" not in board:
+        printBoard(board)
+        print("It's a tie!")
+        gameRunning = False
+
+def checkWin():
+    global gameRunning
+    if checkHorizontal(board) or checkDiagonal(board) or checkRow(board):
+        print(f"The winner is {winner}!")
+        gameRunning = False
+    
+
 # switch the player
 
+def switchPlayer():
+    global currentPlayer
+    if currentPlayer == 'X':
+        currentPlayer = "O"
+    else:
+        currentPlayer = 'X'
+
 # check for win or tie again
+
+while gameRunning:
+    printBoard(board)
+    playerInput(board)
+    checkWin()
+    checkTie(board)
+    switchPlayer()
